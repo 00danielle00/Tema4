84962d81bdd698669da4e2a66359b5220c93e909
DESKTOP-AI1DKKS\Dani <danielleludovicoo@gmail.com>
2024-12-09 21:17:07 +0100

    jeroglificos

--- Tema-4/src/main/java/org/example/Jeroglificos.java
+++ Tema-4/src/main/java/org/example/Jeroglificos.java
@@ -1,14 +1,15 @@
 package org.example;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Jeroglificos {
 
     static Scanner teclado= new Scanner(System.in);
 
-    //2 vectores con valores fijos ya que no van a cambiar
-    static int valores[]={1,10,100,1000,10000,100000,1000000};
-    static char jeroglificos[]={'T','G','C','F','D','R','H'};
+    //Establezco 2 vectores con valores fijos ya que no van a cambiar durante el programa
+    static int valores[]={1000000,100000,10000,1000,100,10,1};
+    static char jeroglificos[]={'H','R','D','F','C','G','T'};
 
     public static void main(String[] args) {
         teclado = new Scanner(System.in);
@@ -18,13 +19,18 @@ public class Jeroglificos {
     }
     public static boolean casoDePrueba() {
         System.out.println("Introduce numeros para pasarlos a jeroglíficos: ");
-        int dato = teclado.nextInt(); //mientras no se introduzca el -1 se ejecutará
-        if (dato==0){
+        int dato =0;
+        try {//implemento un try catch en el caso de que el usuario intente introducir un caracter
+             dato = teclado.nextInt();
+        }catch (InputMismatchException err){
+            System.out.println("ERROR: Introduce un valor válido!");
+            return false;
+        }
+        if (dato==0){// condicion que si el dato introducido es 0 el programa se parará
             return false;
         }else {
             int entrada[] = pedirUsuario(dato);
-            System.out.println("-------");
-
+            
             for (int i = 0; i < entrada.length; i++) {
                int entrad = entrada[i];
                String convertir=repetir(entrad);
@@ -52,7 +58,7 @@ public class Jeroglificos {
         for (int i = 0; i < valores.length ; i++) {
             while (numero>=valores[i]){
                 resultado+=valores[i]; //añadir directamente los valores
-                numero-=valores[i]; // reducimos el número para obtener el siguiente dígito
+                numero-=valores[i]; // reducimos el número para obtener el siguiente dígito a convertir en
             }
         }
         return resultado;
@@ -66,6 +72,7 @@ public class Jeroglificos {
         for (int i = 0; i < veces; i++) {
             resultado2+=digito;
         }
+        System.out.println(resultado2);
         return resultado2;
 
     }
